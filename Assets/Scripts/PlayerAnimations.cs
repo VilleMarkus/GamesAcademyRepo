81777040f851be71fa18f1fe24c45423acae94ac
Ville Raunio <ville.raunio@tuni.fi>
2020-10-04 23:33:34 +0300

    collectables and slight changes to level

---
+++ Assets/Scripts/PlayerAnimations.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator myAnimator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            myAnimator.SetBool("running", true);
+
+        }
+        else
+        {
+            myAnimator.SetBool("running", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) is true)
+        {
+            myAnimator.SetBool("jumping", true);
+        }
+        else
+        {
+            myAnimator.SetBool("jumping", false);
+        }
+    }
+}
